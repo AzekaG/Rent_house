7788bb4c5f69181090c084df9660efb3deb59c52
MatvX_lm73 <MatvX_lm73@student.itstep.org>
2024-05-29 12:35:12 +0300

    Add LocationRepository for working with Location

---
+++ Rent_hose.DAL/Interfaces/IRepositoryLocation.cs
@@ -0,0 +1,50 @@
+﻿using Rent_house.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rent_house.DAL.Interfaces
+{
+    public interface IRepositoryLocation
+    {
+        public Task<IEnumerable<City>> GetCityList();
+
+        public Task<IEnumerable<Country>> GetCountryList();
+
+        public Task<IEnumerable<District>> GetDistrictList();
+
+
+
+        public Task<City> GetCityById(int id);
+
+        public Task<Country> GetCountryById(int id);
+
+        public Task<District> GetDistrictById(int id);
+
+
+
+        public Task CreateCity(City city);
+
+        public Task CreateCountry(Country country);
+
+        public Task CreateDistrict(District district);
+
+
+        public Task DeleteCity(int id);
+
+        public Task DeleteCountry(int id);
+
+        public Task DeleteDistrict(int id);
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Rent_hose.DAL/Repositories/LocationRepository.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rent_house.DAL.EF;
+using Rent_house.DAL.Entities;
+using Rent_house.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rent_house.DAL.Repositories
+{
+    internal class LocationRepository : IRepositoryLocation
+    {
+
+        readonly HouseDbContext _houseDbContext;
+
+        public LocationRepository(HouseDbContext houseDbContext)
+        {
+            _houseDbContext = houseDbContext;
+        }
+
+
+
+        public async Task<IEnumerable<City>> GetCityList() => await _houseDbContext.Cities.ToListAsync();
+       
+
+        public async Task<IEnumerable<Country>> GetCountryList() => await _houseDbContext.Countries.ToListAsync();
+
+
+        public async Task<IEnumerable<District>> GetDistrictList() => await _houseDbContext.Districts.ToListAsync();
+
+
+
+        public async Task<City> GetCityById(int id) => await _houseDbContext.Cities.FirstAsync(x => x.Id == id);
+
+        public async Task<Country> GetCountryById(int id) => await _houseDbContext.Countries.FirstAsync(x => x.Id == id);
+
+        public async Task<District> GetDistrictById(int id) => await _houseDbContext.Districts.FirstAsync(x => x.Id == id);
+
+
+
+        public async Task CreateCity(City city) => await _houseDbContext.Cities.AddAsync(city);
+
+        public async Task CreateCountry(Country country) => await _houseDbContext.Countries.AddAsync(country);
+
+        public async Task CreateDistrict(District district) => await _houseDbContext.Districts.AddAsync(district);
+
+
+        public async Task DeleteCity(int id)
+        {
+            if (await GetCityById(id) != null)
+            {
+                 _houseDbContext.Cities.Remove(await GetCityById(id));
+            }
+            _houseDbContext.SaveChanges();
+        }
+
+        public async Task DeleteCountry(int id)
+        {
+            if (await GetCountryById(id) != null)
+            {
+                _houseDbContext.Countries.Remove(await GetCountryById(id));
+            }
+            _houseDbContext.SaveChanges();
+        }
+
+        public async Task DeleteDistrict(int id)
+        {
+            if (await GetDistrictById(id) != null)
+            {
+                _houseDbContext.Districts.Remove(await GetDistrictById(id));
+            }
+            _houseDbContext.SaveChanges();
+        }
+    }
+}
